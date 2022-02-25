e873910876571e0feefb793263202a0a7d4334ee
Jonry Simbolon <jonrysimbolon19@gmail.com>
2022-02-25 23:19:18 +0700

    - Change all

--- app/src/main/java/com/example/testingapp/TampilanAwal.kt
+++ app/src/main/java/com/example/testingapp/TampilanAwal.kt
@@ -1,15 +1,23 @@
 package com.example.testingapp
 
+import android.content.Intent
 import android.os.Bundle
-import com.example.testingapp.base.BaseActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class TampilanAwal : BaseActivity() {
+class TampilanAwal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tampilan_awal)
 
         goBasicSpecial(DicodingTest::class.java) // For Exercise Dicoding
+    }
+
+    private fun goBasicSpecial(activity: Class<*>) {
+        val intent = Intent(this,activity)
+        startActivity(intent)
+        overridePendingTransition(0,0)
+        finish()
     }
 
     override fun onBackPressed() {
